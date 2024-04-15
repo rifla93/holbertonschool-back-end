4c728a8bfb64c7ecc2bc4bad15cf32ca0acb585d
rifla93 <ralfsemi@yahoo.com>
2024-04-15 13:20:54 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""Module to fetch data from a REST API and display an employee's TODO list progress"""
+
 import requests
 import sys
 
 def get_employee_todo_progress(employee_id):
+    """Fetch and print an employee's TODO list progress
+
+    Args:
+        employee_id (int): The ID of the employee
+
+    Returns:
+        None
+    """
     # Fetch user data
     user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     user_data = user_response.json()
-    employee_name = user_data['name']
+    employee_name = user_data.get('name')
 
     # Fetch todo data
     todos_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
@@ -14,8 +24,8 @@ def get_employee_todo_progress(employee_id):
 
     # Calculate progress
     total_tasks = len(todos_data)
-    done_tasks = len([todo for todo in todos_data if todo['completed']])
-    task_list = [todo['title'] for todo in todos_data if todo['completed']]
+    done_tasks = len([todo for todo in todos_data if todo.get('completed')])
+    task_list = [todo.get('title') for todo in todos_data if todo.get('completed')]
 
     # Print progress
     print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):')
